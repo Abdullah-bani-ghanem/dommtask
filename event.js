69bc3bd10af627d55190e2d2df323e1f211ed19e
Your Name <abdullahbanighanem@gmile.com>
2025-01-04 11:08:58 +0300

    add

---
+++ event.js
@@ -0,0 +1,24 @@
+const form1 = document.getElementById("form1");
+const details = document.getElementById("details");
+
+
+form1.addEventListener("submit",function(events){
+    events.preventDefault(); 
+    const username = document.getElementById("username").value;
+  const age = document.getElementById("age").value;
+  const gender = document.getElementById("gender").value;
+
+  const orderChoice = document.querySelector('input[name="orderChoice"]:checked');
+  if (!orderChoice) {
+    details.textContent = "Please select an order choice.";
+    return;
+  }
+  const selectedOrder = orderChoice.value;
+  details.innerHTML=
+ 
+    <strong>Username:</strong> ${username} <br>
+    <strong>Age:</strong> ${age} <br>
+    <strong>Gender:</strong> ${gender} <br>
+    <strong>Order Choice:</strong> ${selectedOrder}
+  ;}
+);
